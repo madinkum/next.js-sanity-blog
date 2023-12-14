9935e994aeaa3f29c830acfe6c796ac28ed46059
Madinku <MadinkuFinny@gmail.com>
2023-12-14 13:33:39 +0200

    add revalidate

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,11 @@ import Link from "next/link";
 async function getData() {
   const query = `*[_type == "post"]`;
 
-  const data = await client.fetch(query);
+  const data = await client.fetch(query, { 
+    next: {revalidate: 60 },
+    
+    
+  })
 
   return data;
 }
