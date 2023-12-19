fa58763913a1081c08591b55f9f4f32f9178c686
Madinku <MadinkuFinny@gmail.com>
2023-12-19 14:26:55 +0200

    define the code type

--- library/interface.ts
+++ library/interface.ts
@@ -2,9 +2,11 @@ export interface Post {
     title: string;
     overview: string;
     content: any;
+    code: object;
     _id: string;
     slug: {
       current: string;
     };
     _createdAt: string;
+
   }
