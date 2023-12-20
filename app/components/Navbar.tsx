ad5672866d68ee7d322631107121c0d84e1ddc2b
Madinku <MadinkuFinny@gmail.com>
2023-12-20 21:17:46 +0200

    add shallow to links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,14 +6,14 @@ export default function Navbar() {
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between h-16">
         <div className="flex justify-between items-center w-full">
-          <Link href={"/"}>
+          <Link href={"/"} shallow>
             <h1 className="text-2xl font-medium">
               Tech <span className="text-pink-600">Blog</span>
             </h1>
           </Link>
-          <Link href={"/"} className="text-2xl font-medium">Home</Link>
+          <Link href={"/"} shallow className="text-2xl font-medium">Home</Link>
 
-          <Link href={"/blog"} className="text-2xl font-medium">Blog</Link>
+          <Link href={"/blog"} shallow className="text-2xl font-medium">Blog</Link>
           <ThemeButton />
         </div>
       </div>
