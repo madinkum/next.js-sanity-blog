a427e5c106191d0c10e15daa00e5dbbe552240a5
Madinku <MadinkuFinny@gmail.com>
2024-02-01 13:52:03 +0200

    .

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -10,7 +10,6 @@ async function getData(slug: string) {
   const query = `*[_type == "post" && slug.current == "${slug}"][0]`;
 
   const data = await client.fetch(query, {
-    next: { revalidate: 60 },
   }
   );
 
@@ -83,16 +82,16 @@ export default async function SlugPage({
       id="comments"
       repo="madinkum/next.js-sanity-blog"
       repoId="R_kgDOK2zaCg"
-      category="Q&A"
+      category="Annoucements"
       categoryId="DIC_kwDOK2zaCs4Cc1bT"
       mapping="pathname"
-      // term="Welcome to @giscus/react component!"
+      data-strict="0"
       reactionsEnabled="1"
-      emitMetadata="0"
+      emitMetadata="1"
       inputPosition="bottom"
       theme="noborder_light"
       lang="en"
-      loading="lazy"
+      
       
     />
           </div>
