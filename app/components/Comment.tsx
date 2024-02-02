75521d45989017654f77918b316b7120c84f8935
Madinku <MadinkuFinny@gmail.com>
2024-02-02 15:14:26 +0200

    add comment section code

---
+++ app/components/Comment.tsx
@@ -0,0 +1,22 @@
+import {DiscussionEmbed} from "disqus-react"
+  
+const Comments = () => { 
+  const disqusShortname = "madinku-dev-1"
+  
+  const disqusConfig = { 
+    url: "http://localhost:3000", 
+    identifier:'123',  
+    title: "Demo Post" 
+  } 
+  
+  return ( 
+    <div> 
+      <DiscussionEmbed 
+        shortname={disqusShortname} 
+        config={disqusConfig} 
+      /> 
+    </div> 
+  ) 
+} 
+  
+export default Comments;
