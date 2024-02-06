d964d75de3385fe14926cd72b93ff161dd31e9ae
Madinku <MadinkuFinny@gmail.com>
2024-02-06 11:39:30 +0200

    add the giscus env

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
     images: {
         domains:["cdn.sanity.io"],
     },
-
+    env: {
+        NEXT_PUBLIC_GISCUS_REPO: 'next.js-sanity-blog',
+        NEXT_PUBLIC_GISCUS_REPO_ID: 'R_kgDOK2zaCg',
+        NEXT_PUBLIC_GISCUS_CATEGORY: 'Announcements',
+        NEXT_PUBLIC_GISCUS_CATEGORY_ID: 'DIC_kwDOK2zaCs4Cc1bT',
+        NEXT_PUBLIC_GISCUS_MAPPING: 'pathname',
+      },
 }
 
 module.exports = nextConfig
