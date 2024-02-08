a75a804d5834ef0148893728ab9e799b4c01939d
Madinku <MadinkuFinny@gmail.com>
2024-02-08 22:37:47 +0200

    set reactStrictMode to False

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    reactStrictMode: false,
+
     images: {
         domains:["cdn.sanity.io"],
     },
-    async headers() {
-        return [
-            {
-                
-                source: "/blog",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://next-js-sanity-blog-one.vercel.app/blog" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+    
 }
 
 module.exports = nextConfig
