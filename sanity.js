b3fa59b7bb1fa76a4a1e55083fbdf71e9d579a04
Madinku <MadinkuFinny@gmail.com>
2024-02-18 22:10:48 +0200

    .

--- sanity.js
+++ sanity.js
@@ -1,12 +1,12 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
 export const config ={
-    projectId,
+    projectId:"dbfhkj94",
     dataset,
     apiVersion,
     useCdn:true,
