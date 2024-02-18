5890b0b04749028a2ed18e1263d6460457f7d877
Madinku <MadinkuFinny@gmail.com>
2024-02-18 21:02:03 +0200

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Header from "@/components/Header";
 
-
-
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     
