180f70f5b12856fb30a3ee33f89c9fc95fa761f4
Madinku <MadinkuFinny@gmail.com>
2024-02-20 16:27:13 +0200

    change a few of the css styles

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -11,7 +11,7 @@ const blockPage = ({ posts }: Props) => {
   return (
     <div>
       
-      <div className="font-bodyFont max-w-3xl mx-auto mb-10 divide-y divide-gray-200 dark:divide-gray-700  border-b-[1px] space-y-2" >
+      <div className="font-bodyFont max-w-3xl mx-auto mb-10 divide-gray-200 dark:divide-gray-700  border-b-[1px] space-y-2" >
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900  sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
             ALL BLOG POSTS
@@ -22,14 +22,13 @@ const blockPage = ({ posts }: Props) => {
             <Link key={post._id} href={`/post/${post.slug.current}`}>
               <div className="h-2/5 w-full flex flex-col justify-center">
                 <div
-                  className=" px-4 py-1]
-              border-b-gray-500"
+                  className=" px-4 [py-1] border-b-gray-500"
                 >
                   <p className="text-base font-medium leading-6 text-pink-500">
                     {new Date(post.publishedAt).toISOString().split("T")[0]}
                   </p>
                   <div>
-                    <h2 className="text-3xl">{post.title}</h2>
+                    <h2 className="text-2xl font-bold">{post.title}</h2>
                   </div>
                 </div>
                 <p className="py-2 px-4 text-base">
