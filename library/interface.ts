d13a0b85a3dd6febbf83a5e438c54fb412c0470d
Madinku <MadinkuFinny@gmail.com>
2024-02-27 14:08:53 +0200

    add interface fo =r comments

--- library/interface.ts
+++ library/interface.ts
@@ -8,4 +8,22 @@ export interface Post {
       current: string;
     };
     _createdAt: string;
+    comments:Comment[];
   }
+
+
+  export interface Comment {
+    approved: boolean;
+    comment: string;
+    email: string;
+    name: string;
+    post: {
+      _ref: string;
+      _type: string;
+    };
+    _createdAt:string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
+    }
