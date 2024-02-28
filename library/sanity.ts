f8ed726060f96e3011b6f4763eb27c70ba78a8fb
Madinku <MadinkuFinny@gmail.com>
2024-02-28 21:24:39 +0200

    addthe projectId

--- library/sanity.ts
+++ library/sanity.ts
@@ -5,7 +5,7 @@ const projectId =process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset =process.env.NEXT_PUBLIC_SANITY_DATASET ;
 
 const config={
-    projectId,
+    projectId:"dbfhkj94",
     dataset,
     apiVersion:"2024-02-15",
     useCdn:true,
