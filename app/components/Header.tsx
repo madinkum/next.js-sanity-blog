465ed4c92a044f0e687d7dca4879468c364ed379
Madinku <MadinkuFinny@gmail.com>
2024-03-04 12:44:37 +0200

    move the components to app folder

---
+++ app/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import ThemeButton from "./ThemeButton";
+
+
+const Header = () => {
+ 
+  return (
+    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+     
+     <div className="flex justify-between h-16">
+        <div className="flex justify-between items-center w-full">
+          <Link href={"/"} shallow>
+            <h1 className="text-2xl font-medium">
+              Tech <span className="text-pink-600">Blog</span>
+            </h1>
+          </Link>
+          <Link href={"/"} shallow className="text-2xl font-medium">Home</Link>
+
+          <Link href={"/blog"} shallow className="text-2xl font-medium">Blog</Link>
+          <ThemeButton />
+        </div>
+      </div>
+       
+      
+    </div>
+  );
+};
+
+export default Header;
