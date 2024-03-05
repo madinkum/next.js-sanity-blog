193a2abe21ead7429c40cab197c47e6b544e7a11
Madinku <MadinkuFinny@gmail.com>
2024-03-05 14:19:19 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}
+        className={`${inter.className} bg-white text-black dark:bg-gray-900  dark:text-white h-full `}
       >
         <Providers>
           <Header />
--- app/studio/[[...index]]/index.tsx
+++
@@ -1,6 +0,0 @@
-import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity.config";
-
-export default function Studio() {
-  return <NextStudio config={config} />;
-}
