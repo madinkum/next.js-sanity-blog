7a9e123071adedc6b91144c18155d5ad7904eba0
Madinku <MadinkuFinny@gmail.com>
2024-03-22 10:22:42 +0200

    import reply schema in the index

--- personalblog/schemaTypes/index.ts
+++ personalblog/schemaTypes/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import comment from './comment'
+import reply from './reply'
 
-export const schemaTypes = [post, author, category,comment, blockContent]
+export const schemaTypes = [post, author, category,comment, blockContent, reply]
