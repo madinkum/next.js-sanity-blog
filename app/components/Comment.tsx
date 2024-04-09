ab485f1dce54289e5dad9ac74886e1b5c2e15383
Madinku <MadinkuFinny@gmail.com>
2024-04-09 17:41:46 +0200

    add a fastcomment component

---
+++ app/components/Comment.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import {FastCommentsCommentWidget} from 'fastcomments-react'
+
+const Comment = () => {
+  return (
+    <div>
+      <FastCommentsCommentWidget tenantId="jMXB-nOiyjaL" />
+  
+    </div>
+  )
+}
+
+export default Comment
