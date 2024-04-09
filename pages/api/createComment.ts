8a2205001de0800bbe05ee6f05ced51de3f57048
Madinku <MadinkuFinny@gmail.com>
2024-04-09 13:04:46 +0200

    add api logic for comments

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -13,22 +13,40 @@ export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log(req.body)
-    const {_id,name,email,comment}= JSON.parse(req.body);
-    try{
-        await client.create({
-          _type: 'comment',
-          post:{
-              _type: 'reference',
-              _ref: _id
-          },
-          name,
-          email,
-          comment
-      });
-    }catch(err){
-        return res.status(500).json({message: "Could not submit comment", err});
+    const { _id, name, email, comment, parentId } = JSON.parse(req.body);
+    try {
+        if (parentId) {
+            // Handle reply
+            await client.create({
+                _type: 'comment',
+                post: {
+                    _type: 'reference',
+                    _ref: _id
+                },
+                parentComment: {
+                    _type: 'reference',
+                    _ref: parentId
+                },
+                name,
+                email,
+                comment
+            });
+        } else {
+            // Handle regular comment
+            await client.create({
+                _type: 'comment',
+                post: {
+                    _type: 'reference',
+                    _ref: _id
+                },
+                name,
+                email,
+                comment
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: "Could not submit comment", err });
     }
     console.log("Comment Submitted")
-  return res.status(200).json({ message: 'Comment Submitted' });
+    return res.status(200).json({ message: 'Comment Submitted' });
 }
