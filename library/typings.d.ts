b1406b74f6e8362cabac33bee64360ff19498121
Madinku <MadinkuFinny@gmail.com>
2024-04-09 13:03:20 +0200

    modify types

--- library/typings.d.ts
+++ library/typings.d.ts
@@ -1,50 +1,54 @@
 export interface Post {
   code: string;
   post: Post;
-   _id:string;
-   publishedAt:string;
-   title:string;
-   author:{
-    name:string;
-    image:string;
-   }
-   comments:Comment[];
-description:string;
-mainImage:{
-    asset:{
-        url:string;
+  _id: string;
+  publishedAt: string;
+  title: string;
+  author: {
+    name: string;
+    image: string;
+  };
+  comments: Comment[];
+  description: string;
+  mainImage: {
+    asset: {
+      url: string;
     };
-};
-slug:{
-    current:string;
-};
-body:[object];
-
-
+  };
+  slug: {
+    current: string;
+  };
+  body: [object];
+  replies: Reply[];
 }
 
 export interface Comment {
-    approved: boolean;
-    comment: string;
-    email: string;
-    name: string;
-    post: {
-      _ref: string;
-      _type: string;
-    };
-    publishedAt:string;
-    _id: string;
-    _rev: string;
+  approved: boolean;
+  comment: string;
+  email: string;
+  name: string;
+  post: {
+    _ref: string;
     _type: string;
-    _updatedAt: string;
-    replies: Reply[];
-    reply_of:string;
-    }
+  };
+  publishedAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+  
+  replies?: Comment[];
+  parentCommentId?: string;
+}
 
-    export interface Reply {
-      _id: string;
-      name: string;
-      email: string;
-      comment: string;
 
-    }
+export interface Reply {
+  reply: string;
+  _id: string;
+  name: string;
+  email: string;
+  comment: {
+    _ref: string;
+    _type: string;
+  };
+}
