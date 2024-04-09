4d6bc1aa266a31916ee8d684b759c700da94f161
Madinku <MadinkuFinny@gmail.com>
2024-04-09 13:01:54 +0200

    add disqus comment section

---
+++ app/components/DisqusComments.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { Post } from "@/library/typings"
+import {DiscussionEmbed} from "disqus-react"
+
+
+
+const DisqusComments = (slug:string, post:Post) => { 
+const disqusShortname = 'madinku-dev-3'
+
+const disqusConfig = { 
+	url: "http://madinku.dev" +'/blog' +slug , 
+	identifier: slug, 
+	title: post.title,
+} 
+ 
+return ( 
+	<div> 
+	<DiscussionEmbed 
+		shortname={disqusShortname} 
+		config={disqusConfig} 
+	/> 
+	</div> 
+) 
+} 
+
+export default DisqusComments;
